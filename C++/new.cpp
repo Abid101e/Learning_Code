d261b43d0e8b6a206c17a02ab22a5b607b687409
Md. Abid Hasan <emonff89@gmail.com>
2024-10-03 23:56:37 +0600

    Started learning python today

---
+++ C++/new.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+    std::cout << "You entered: " << number << std::endl;
+
+    return 0;
+}
---
+++ Python/Chapter-1 -ps/problem3.py
@@ -0,0 +1,7 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I love using Python")
+engine.runAndWait()
+
+# this module is used to convert text to speech
+# we have imported the pyttsx3 module
